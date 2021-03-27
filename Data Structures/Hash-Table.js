51ef5085e85c0739e59b2f3f1887f54de0e370d7
George Lowe <georgeloweone@gmail.com>
2021-03-27 17:58:12 +0000

    Added checkExists(key) and removeItem(key)

--- Data Structures/Hash-Table.js
+++ Data Structures/Hash-Table.js
@@ -1,19 +1,21 @@
+// Convert the key to an integer to be used as the table index
 function hashStringToInt(string, tableSize) {
-  // start at a prime num to spread out where keys are stored --> randomisation for O(1)
+  // Start on a prime to spread where keys are stored --> randomisation O(1)
   let hash = 17;
 
   for (let i = 0; i < string.length; i++) {
     hash = hash + string.charCodeAt(i);
   }
-
   return hash % tableSize;
 }
 
 class HashTable {
   table = new Array(100);
 
+  // Set a key and value pair within the table
   setItem = (key, value) => {
     const index = hashStringToInt(key, this.table.length);
+
     if (this.table[index]) {
       this.table[index].push([key, value]);
     } else {
@@ -21,17 +23,44 @@ class HashTable {
     }
   };
 
+  // Return the value of an item given a key
   getItem = (key) => {
     const index = hashStringToInt(key, this.table.length);
-
-    if (!this.table[index]) {
-      return null;
-    } else {
+    if (this.table[index] && this.table[index].find((x) => x[0] === key)) {
       return this.table[index].find((x) => x[0] === key)[1];
+    } else {
+      return null;
     }
   };
 
-  // To implement:
-  // exists method
-  // remove method
+  // Check that a key exists in the table
+  checkExists = (key) => {
+    const index = hashStringToInt(key, this.table.length);
+    if (this.table[index]) {
+      return this.table[index].find((x) => x[0] === key) ? true : false;
+    } else {
+      return false;
+    }
+  };
+
+  // Remove an item and return it
+  removeItem = (key) => {
+    const index = hashStringToInt(key, this.table.length);
+
+    if (!this.table[index]) {
+      console.log("success");
+      return null;
+    } else {
+      var del = this.table[index].indexOf(
+        this.table[index].find((x) => x[0] === key)
+      );
+      return this.table[index].splice(del, 1);
+    }
+  };
+
+  // Helper function: show list of collisions
+  showLinkedList = (key) => {
+    const index = hashStringToInt(key, this.table.length);
+    return this.table[index];
+  };
 }
